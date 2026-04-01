8b360927273349463f731092805bc10f1fb9a79f
saikrishnabodapatiaja <S.Bodapati@ajacs.in>
2026-04-01 11:20:37 +0530

    intial spring boot setup

---
+++ src/main/java/com/src/sprintflow/SprintflowApplication.java
@@ -0,0 +1,13 @@
+package com.src.sprintflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/src/sprintflow/SprintflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.src.sprintflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
